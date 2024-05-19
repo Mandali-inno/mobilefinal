c8d7e477da61efc0e18b52220e27c280b17d050d
Mandali-inno <119554576+Mandali-inno@users.noreply.github.com>
2024-05-19 14:44:49 +0200

    Delete sensormobileapplication-main directory

--- sensormobileapplication-main/android/app/src/main/kotlin/com/example/sensormobileapplication/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mobile_final
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- sensormobileapplication-main/lib/components/consts.dart
+++
@@ -1,1 +0,0 @@
-const String GOOGLE_MAPS_API_KEY = "AIzaSyDDqJXmDSbO_ic7xF-X4N76RXakC0Mtioc";
